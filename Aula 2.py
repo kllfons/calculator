cca4870f353b4275120a8ac77923207c513d299d
Kamilla Fonseca <kllfons04@gmail.com>
2022-07-07 22:19:52 -0300

    Add files via upload

---
+++ Aula 2.py
@@ -0,0 +1,19 @@
+a= int(input('entre com o primeiro valor:'))
+b= int(input('entre com o segundo valor:'))
+print(type(a))
+soma= a+b
+subtração= a-b
+multiplicação= a*b
+divisão= a/b
+resto= a%b
+print('soma: {soma}''\nsubtração:{sub}.'
+      '\nmultiplicação:{multi}'
+      '\ndivisão:{divi}'
+      '\nresto:{resto}'.format (soma=soma,
+                                sub=subtração, divi=divisão,
+                                resto=resto, multi=multiplicação))
+print('soma; '+str (soma))
+# x='1'
+# soma2= int(x)+1
+# print(soma2)
+
